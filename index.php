5f9459cf9b6707bf6370109bb38d60475d4b23f0
goshgarmirzayev <qosqarmirze@gmail.com>
2019-09-21 04:12:33 +0400

    Ready for environment

---
+++ index.php
@@ -0,0 +1,140 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+
+
+  <!--   <link href="//fonts.googleapis.com/css?family=Roboto:400,500,700" rel="stylesheet"/>  -->
+
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+  <title>Doctor Review</title>
+</head>
+<style>
+    .bg-header{
+           background:  linear-gradient(rgba(0,169,154, 0.89),rgba(0,169,154, 0.89));
+       }
+   
+   </style>
+   <body>
+   
+     <div class="container-fluid bg-header py-2">
+           <div class="row">
+               <div class="py-0  col-lg-12 ">
+                   
+                   <h4 class="text-center text-white pt-0 py-2"> <i class="fas fa-stethoscope"></i> Doctor Review</h4>
+               </div>
+           </div>
+       </div>
+
+
+
+
+ <div class="container-fluid">
+  <div class="row">
+    <div class="d-block mx-auto">
+      <div class="card col-xs-8 col-sm-7 col-md-6
+      col-lg-6  mx-auto my-2 d-block" >
+      <div class="card-body ">
+        <h5 class="card-title"><i class="fas fa-check-circle text-info mr-2"></i>Prof. Dr. Sezai Leventoğlu </h5>
+        <h6 class="card-subtitle mb-2 text-muted">
+        Genel Cerrahi, Gastroenteroloji Cerrahisi </h6>
+        <p   class="card-text"  style="font-family: Roboto,Helvetica Neue,Helvetica,Arial,sans-serif;"><i class="fas fa-map-marker-alt text-info mr-2"></i>The Paragon Tower, Kızılırmak Mah. No:3 Kat:10, Daire: 46 Çukurambar, Ankara Prof. Dr. Sezai Leventoğlu Muayenehanesi </p>
+
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+
+
+<div class="container-fluid">
+  <div class="row">
+    <div class="d-block mx-auto">
+      <div class="card col-xs-8 col-sm-7 col-md-6
+      col-lg-6  mx-auto my-2 d-block" >
+      <div class="card-body ">
+        <h5 class="card-title"><i class="fas fa-check-circle text-info mr-2"></i>Prof. Dr. Sezai Leventoğlu </h5>
+        <h6 class="card-subtitle mb-2 text-muted">
+        Genel Cerrahi, Gastroenteroloji Cerrahisi </h6>
+        <p   class="card-text"  style="font-family: Roboto,Helvetica Neue,Helvetica,Arial,sans-serif;"><i class="fas fa-map-marker-alt text-info mr-2"></i>The Paragon Tower, Kızılırmak Mah. No:3 Kat:10, Daire: 46 Çukurambar, Ankara Prof. Dr. Sezai Leventoğlu Muayenehanesi </p>
+
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+
+
+<div class="container-fluid">
+  <div class="row">
+    <div class="d-block mx-auto">
+      <div class="card col-xs-8 col-sm-7 col-md-6
+      col-lg-6  mx-auto my-2 d-block" >
+      <div class="card-body ">
+        <h5 class="card-title"><i class="fas fa-check-circle text-info mr-2"></i>Prof. Dr. Sezai Leventoğlu </h5>
+        <h6 class="card-subtitle mb-2 text-muted">
+        Genel Cerrahi, Gastroenteroloji Cerrahisi </h6>
+        <p   class="card-text"  style="font-family: Roboto,Helvetica Neue,Helvetica,Arial,sans-serif;"><i class="fas fa-map-marker-alt text-info mr-2"></i>The Paragon Tower, Kızılırmak Mah. No:3 Kat:10, Daire: 46 Çukurambar, Ankara Prof. Dr. Sezai Leventoğlu Muayenehanesi </p>
+
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+
+
+
+<div class="container-fluid">
+  <div class="row">
+    <div class="d-block mx-auto">
+      <div class="card col-xs-8 col-sm-7 col-md-6
+      col-lg-6  mx-auto my-2 d-block" >
+      <div class="card-body ">
+        <h5 class="card-title"><i class="fas fa-check-circle text-info mr-2"></i>Prof. Dr. Sezai Leventoğlu </h5>
+        <h6 class="card-subtitle mb-2 text-muted">
+        Genel Cerrahi, Gastroenteroloji Cerrahisi </h6>
+        <p   class="card-text"  style="font-family: Roboto,Helvetica Neue,Helvetica,Arial,sans-serif;"><i class="fas fa-map-marker-alt text-info mr-2"></i>The Paragon Tower, Kızılırmak Mah. No:3 Kat:10, Daire: 46 Çukurambar, Ankara Prof. Dr. Sezai Leventoğlu Muayenehanesi </p>
+
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+
+
+
+
+
+
+<div class="container-fluid">
+  <div class="row">
+    <div class="text-center mx-auto mt-3 py-3 "  >
+      <h5>Axtardığınız həkim tapılmadısa sorğu göndərin</h5>
+      <button type="button" class="btn btn-lg bg-header rounded-pill font-weight-bold shadow text-white mt-2 pl-5  pr-5 py-2" >Sorğu göndər</button>
+    </div>
+  </div>  
+</div>
+
+
+
+
+
+
+
+
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+</html>
